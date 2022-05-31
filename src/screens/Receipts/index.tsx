529ef70b4c89d596f30837ddcd11191aa605b438
luanfv <luanfrancavieira@gmail.com>
2022-05-31 09:25:43 -0300

    feat: get info of the image

--- src/screens/Receipts/index.tsx
+++ src/screens/Receipts/index.tsx
@@ -10,10 +10,14 @@ import { File, FileProps } from '../../components/File';
 export function Receipts() {
   const [photos, setPhotos] = useState<FileProps[]>([]);
   const [photoSelected, setPhotoSelected] = useState('');
+  const [photoInfo, setPhotoInfo] = useState('');
 
   async function handleShowImage(path: string) {
     const urlImage = await storage().ref(path).getDownloadURL();
     setPhotoSelected(urlImage);
+
+    const info = await storage().ref(path).getMetadata();
+    setPhotoInfo(`Upload realizazdo em ${info.timeCreated}`);
   }
 
 
@@ -42,7 +46,7 @@ export function Receipts() {
       <Photo uri={photoSelected} />
 
       <PhotoInfo>
-        Informações da foto
+        {photoInfo}
       </PhotoInfo>
 
       <FlatList
